55ad5e8909cf727078d1198c1a89b03f98205eb5
LAPTOP-HJ9F78UR\saifi <saifilmomin4@gmail.com>
2021-04-01 18:52:57 -0400

    changes in gitignore

--- src/components/daily-weather.tsx
+++ src/components/daily-weather.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import { DailyWeather } from '../interfaces/daily-weather'
+import { Table, Container, Row, Col } from 'react-bootstrap';
 
 interface Props {
     data: Array<DailyWeather> | undefined
@@ -9,32 +9,41 @@ interface Props {
 const DailyWeatherComponent: React.FC<Props> = ({ data }) => {
     return (
         <>
-            {
-                data ?
-                    <div className="weekly-container">
-                        <table style={{ color: 'white' }}>
-                            <tbody>
-                                {data.map((day) => (
-                                    <tr>
-                                        <td>
-                                            {day.Day}
-                                        </td>
-                                        <td>
-                                            <img alt={day.Description} width="50px" src={`http://openweathermap.org/img/wn/${day.Icon}@2x.png`} />
-                                            <span>{day.Description}</span>
-                                        </td>
-                                        <td>
-                                            <span>{day.MinTemp}<sup>o </sup></span>
-                                            <span>{day.MaxTemp}<sup>o</sup></span>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                    :
-                    <div>Loading</div>
-            }
+            <Container>
+                <Row>
+                    <Col md={3}></Col>
+                    <Col md={6}>
+                        {
+                            data ?
+                                <div className="weekly-container">
+                                    <Table style={{ color: 'white' }}>
+                                        <tbody>
+                                            {data.map((day) => (
+                                                <tr>
+                                                    <td>
+                                                        {day.Day}
+                                                    </td>
+                                                    <td>
+                                                        <img alt={day.Description} width="50px" src={`http://openweathermap.org/img/wn/${day.Icon}@2x.png`} />
+                                                        <span>{day.Description}</span>
+                                                    </td>
+                                                    <td>
+                                                        <span>{day.MinTemp}<sup>o </sup></span>
+                                                        <span>{day.MaxTemp}<sup>o</sup></span>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </Table>
+                                </div>
+                                :
+                                <div>Loading</div>
+                        }
+                    </Col>
+                    <Col md={3}></Col>
+                </Row>
+            </Container>
+
         </>
     )
 }
--- src/components/header-component.tsx
+++ src/components/header-component.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import debounce from 'lodash/debounce';
 
 const Header: React.FC = () => {
-    // const [search, setSearch] = useState('');
+    const [search, setSearch] = useState('');
+
+    const callApi = useCallback(debounce((searchkey) => getLatLong(searchkey), 1000), []);
+
+    const handleChange = (e: any) => {
+        setSearch(e.target.value);
+        callApi(e.target.value);
+    }
+
+    const getLatLong = (searchkey: string) => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchkey}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+            .then(data => data.json())
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => console.log(err));
+    }
+
     return (
         <>
-            <div className="header">
+            <div onChange={e => handleChange(e)} className="header">
                 <input className="search" type="text" />
             </div>
         </>
